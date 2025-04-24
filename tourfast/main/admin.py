064f7863d4ef1aba75399327c4d8efaa55e9b576
JohnnySuon <ValentSuon@gmail.com>
2025-04-24 13:59:50 +0300

    fix

--- tourfast/main/admin.py
+++ tourfast/main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Country, Clients, Hotel, Tour, Booking, Buyer, Contracts
+from .models import Country, Clients, Hotel, Tour, Booking, Buyer, Contracts, Transaction
 
 admin.site.register(Country)
 admin.site.register(Clients)
@@ -8,5 +8,6 @@ admin.site.register(Tour)
 admin.site.register(Booking)
 admin.site.register(Buyer)
 admin.site.register(Contracts)
+admin.site.register(Transaction)
 
 
